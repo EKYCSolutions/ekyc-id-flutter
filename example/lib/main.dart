04a868ddf0b22f5ea61269f9c7324ebf669993bc
Socret360 <lee.socret@gmail.com>
2022-06-26 14:44:03 +0700

    added ekyc id express view

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,15 @@
-import 'package:ekyc_id_flutter/core/models/language.dart';
 import 'package:ekyc_id_flutter/core/document_scanner/document_scanner_result.dart';
-import 'package:ekyc_id_flutter/core/document_scanner/document_scanner_view.dart';
+import 'package:ekyc_id_flutter/core/models/language.dart';
+import 'package:ekyc_id_flutter/core/services.dart';
+import 'package:ekyc_id_flutter/ekyc_id_express.dart';
 import 'package:flutter/material.dart';
+import 'package:ekyc_id_flutter/core/liveness_detection/liveness_detection_result.dart';
+import 'package:ekyc_id_flutter/core/widgets/kyc_result_card.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  EkycIDServices.instance.setURL("https://eekyc-server.demo.ekycsolutions.com");
   runApp(MyApp());
 }
 
@@ -18,26 +24,56 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
-  Future<void> onDocumentScanned({
-    DocumentScannerResult? mainSide,
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> onKYCCompleted({
+    required LivenessDetectionResult liveness,
+    required DocumentScannerResult mainSide,
     DocumentScannerResult? secondarySide,
   }) async {
-    print("== Result ==");
+    await showCupertinoModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return KYCResultCard(
+          mainSide: mainSide,
+          secondarySide: secondarySide,
+          livenessDetectionResult: liveness,
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('EkycID Flutter'),
-        ),
-        body: Container(
-          color: Colors.blue,
-          child: DocumentScannerView(
-            onDocumentScanned: onDocumentScanned,
-            language: Language.KH,
-          ),
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            await showCupertinoModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return EkycIDExpress(
+                  language: Language.KH,
+                  onKYCCompleted: onKYCCompleted,
+                );
+              },
+            );
+          },
+          child: Text("Start KYC"),
         ),
       ),
     );
