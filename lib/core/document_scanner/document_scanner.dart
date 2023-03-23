04ea3b38c6f01cfb27d961ddab78bd21e30a44b2
= <=>
2023-03-23 17:34:42 +0700

    add platform view link

--- lib/core/document_scanner/document_scanner.dart
+++ lib/core/document_scanner/document_scanner.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'document_scanner_values.dart';
 import 'document_scanner_controller.dart';
@@ -28,11 +32,39 @@ class _DocumentScannerState extends State<DocumentScanner> {
     StandardMessageCodec decorder = const StandardMessageCodec();
 
     if (Platform.isAndroid) {
-      return AndroidView(
+      return PlatformViewLink(
         viewType: 'DocumentScanner',
-        onPlatformViewCreated: onPlatformViewCreated,
-        creationParamsCodec: decorder,
+        surfaceFactory: (context, controller) {
+          log("surfaceFactory");
+          return AndroidViewSurface(
+            controller: controller as AndroidViewController,
+            gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+            hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+          );
+        },
+        onCreatePlatformView: (params) {
+          log("onCreatePlatformView");
+          const Map<String, dynamic> creationParams = <String, dynamic>{};
+
+          return PlatformViewsService.initSurfaceAndroidView(
+            id: params.id,
+            viewType: 'DocumentScanner',
+            layoutDirection: TextDirection.ltr,
+            creationParams: creationParams,
+            creationParamsCodec: const StandardMessageCodec(),
+            onFocus: () {
+              params.onFocusChanged(true);
+            },
+          )
+            ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+            ..create();
+        },
       );
+      // return AndroidViewSurface(
+      //   viewType: 'DocumentScanner',
+      //   onPlatformViewCreated: onPlatformViewCreated,
+      //   creationParamsCodec: decorder,
+      // );
     }
 
     if (Platform.isIOS) {
